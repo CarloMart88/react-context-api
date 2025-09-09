6aa79a6ee4a5920512d3a23603b732b1d5f448c5
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-09 15:37:04 +0200

    feat: milestone1

---
+++ src/contexts/BudgetContext.jsx
@@ -0,0 +1,11 @@
+import { createContext, useState, useContext } from "react";
+
+// creo il contesto
+const budgetContext = createContext();
+
+// ora il provider
+const BudgetProvider = ({ childern }) => {
+  return <div></div>;
+};
+
+export default BudgetProvider;
