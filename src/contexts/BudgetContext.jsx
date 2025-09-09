61dca311d6073b9d9c0903e19c15951cbf8b8ca3
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-09 16:09:11 +0200

    feat: budgetContext.jsx with context provider and customhook

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -5,7 +5,21 @@ const budgetContext = createContext();
 
 // ora il provider
 const BudgetProvider = ({ childern }) => {
-  return <div></div>;
+  //Deve contenere uno stato budgetMode di tipo booleano (true/false)
+  //Deve fornire anche la funzione per modificarlo (setBudgetMode)
+  const [budgetMode, setbudgetMode] = useState(false);
+
+  const value = { budgetMode, setbudgetMode };
+
+  return <budgetContext value={value}>{childern}</budgetContext>;
 };
 
-export default BudgetProvider;
+// definisco l'hook usare il contesto
+
+const useBudget = () => {
+  const context = useContext(budgetContext);
+  return context;
+};
+
+// ed esporto tutto
+export default [BudgetProvider, useBudget];
